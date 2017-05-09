31c92e1c728b0ce485b34ab20f21581cde441778
Thibault Févry <ThibaultFevry@gmail.com>
2017-05-09 13:39:34 +0200

    Separate models from rest of code.
    This should make it easier to experiment with different models.
    Any model in cnn_models should provide a model and an optimizer

--- cifar10_cnn.py
+++ cifar10_cnn.py
@@ -16,6 +16,8 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 import numpy as np
 
+from cnn_models.fchollet_example_cnn import model, opt
+
 batch_size = 32
 num_classes = 10
 epochs = 200
@@ -36,32 +38,7 @@ print(x_test.shape[0], 'test samples')
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-model = Sequential()
-
-model.add(Conv2D(32, (3, 3), padding='same',
-                 input_shape=x_train.shape[1:]))
-model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(64, (3, 3), padding='same'))
-model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes))
-model.add(Activation('softmax'))
-
-# initiate RMSprop optimizer
-opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+## Model and optimizer imported in imports.
 
 # Let's train the model using RMSprop
 model.compile(loss='categorical_crossentropy',
---
+++ cnn_models/fchollet_example_cnn.py
@@ -0,0 +1,39 @@
+import keras
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+import numpy as np
+
+
+# Model 1 : F.Chollet's example CNN
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), padding='same',
+                 input_shape=(32, 32, 3)))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+
+# initiate RMSprop optimizer
+opt = keras.optimizers.rmsprop(lr=0.001, decay=1e-6)
+
+
